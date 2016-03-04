20ccc0456a4278dcc708c855a55f48acc8c31f10
sagem007 <fly-sanya@yandex.ru>
2016-03-05 03:48:31 +0600

    Работа дома
    Добавлены методы работы через модуль Lists

--- db.erl
+++ db.erl
@@ -3,16 +3,26 @@
 
 new() -> [].
 
-delete(DK, L) -> [{K, V} || {K, V} <- L, K =/= DK].
+%delete(DK, L) -> [{K, V} || {K, V} <- L, K =/= DK].
 
 put(K, V, L) -> 
     L1 = delete(K, L),
     [{K, V}|L1].
 
-get(_, []) -> {error, no_found};
-get(K, [{K, V}|_]) -> V;
-get(FK, [{_, _}|T]) -> get(FK, T).
+%get(_, []) -> {error, not_found};
+%get(K, [{K, V}|_]) -> V;
+%get(FK, [{_, _}|T]) -> get(FK, T).
 
-find(FV, L) -> [K || {K, V} <- L, V == FV].        
+%find(FV, L) -> [K || {K, V} <- L, V == FV].
 
-%Использовать (где можно) модуль Lists
+delete(K, L) -> 
+    lists:keydelete(K, 1, L).
+
+get(K, L) ->
+    case lists:keyfind(K, 1, L) of
+		{_, Value} -> Value;
+		_ -> {error, 'not_found'}
+	end.
+
+find(V, L) -> 
+    lists:filter(fun({_, V1}) -> V =:= V1 end, L).
